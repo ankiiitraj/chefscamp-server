be60406894c513acea324fbc36fa73b690d32edb
Ankit Raj <ankitatiiitr@gmail.com>
2020-11-26 23:47:41 +0530

    completed implementation of private tags related routes and logic

--- src/controller/tags.controller.php
+++ src/controller/tags.controller.php
@@ -84,7 +84,14 @@ function create_private_tag($username, $tag){
 }
 
 // Add tag to problem
-function add_tag_to_problem($username, $problemCode, $tag){
+function add_tag_to_problem(
+    $username, 
+    $problemCode, 
+    $tag, 
+    $successfulSubmissions, 
+    $totalSubmissions, 
+    $problemTags
+){
     include __DIR__."/../util/dbconnect.php";
     if($mysqli->connect_error){
         die("Connection failed: " . $mysqli->connect_error);
@@ -99,9 +106,26 @@ function add_tag_to_problem($username, $problemCode, $tag){
             if($stmt->num_rows == 0){
                 $stmt->close();
                 $id = uniqid("id", true);
-                $query = "INSERT INTO problems(tagId, problemCode, id) VALUES((SELECT tagId FROM tags WHERE username=? AND tag=?) ,?,?)";
+                $query = "INSERT INTO 
+                problems(
+                    tagId, 
+                    problemCode, 
+                    id, 
+                    successfulSubmissions, 
+                    totalSubmissions, 
+                    problemTags
+                ) VALUES((SELECT tagId FROM tags WHERE username=? AND tag=?) ,?,?,?,?,?)";
                 $stmt = $mysqli->prepare($query);
-                $stmt->bind_param("ssss", $username, $tag, $problemCode, $id);
+                $stmt->bind_param(
+                    "sssssss", 
+                    $username, 
+                    $tag, 
+                    $problemCode, 
+                    $id,
+                    $successfulSubmissions, 
+                    $totalSubmissions, 
+                    $problemTags
+                );
                 if($stmt->execute()){
                     $stmt->close();
                     $query = "UPDATE tags SET count = count +1 WHERE username=? AND tag=?";
@@ -138,45 +162,86 @@ function add_tag_to_problem($username, $problemCode, $tag){
     }
 }
 
-// function get_problems_by_private_tags($username, $tags){
-//     include __DIR__."/../util/dbconnect.php";
-//     if($mysqli->connect_error){
-//         die("Connection failed: " . $mysqli->connect_error);
-//         return get_error_arr("DB connection failed");
-//     }
-//     try{
-//         $query = "SELECT * FROM (SELECT tagId FROM tags WHERE username=? AND tag=);
-//         $stmt = $mysqli->prepare($query);
-//         $stmt->bind_param("sss", $tagId, $problemCode, $id);
-//         if($stmt->execute()){
-//             $stmt->close();
-//             $query = "UPDATE tags SET count = count +1 WHERE tagId=?";
-//             $stmt = $mysqli->prepare($query);
-//             $stmt->bind_param("s", $tagId);
-//             if($stmt->execute()){
-//                 $stmt->close();
-//                 $mysqli->close();
-//                 return ["status"=>"OK"];
-//             }else{
-//                 $stmt->close();
-//                 $mysqli->close();
-//                 return get_error_arr("DB query failed");    
-//             }
-//         }else{
-//             $stmt->close();
-//             $mysqli->close();
-//             return get_error_arr("DB query failed");
-//         }
-//     }catch(mysqli_sql_exception $e){
-//         $stmt->close();
-//         $mysqli->close();
-//         return get_error_arr("DB error while adding tag!");
-//     }  
-// }
+function get_problems_by_private_tags($username, $tags, $offset){
+    include __DIR__."/../util/dbconnect.php";
+    if($mysqli->connect_error){
+        die("Connection failed: " . $mysqli->connect_error);
+        return get_error_arr("DB connection failed");
+    }
+    try{
+        $tags = explode(",", $tags);
+        $query_tags = "";
+        foreach($tags as $tag){
+            $query_tags .= "'".$tag."',";
+        }
+        $query_tags = substr($query_tags, 0, -1);
+        $query = 
+        "SELECT * FROM problems WHERE tagId IN (SELECT tagId FROM tags WHERE tag IN (".$query_tags.") AND userName='".$username."') GROUP BY problemCode HAVING COUNT(tagId) = ".count($tags)." LIMIT 20 OFFSET ".strval($offset).";";
+        
+        $stmt = $mysqli->prepare($query);
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+            $res_arr = ["status"=>"OK", "result"=>["data"=>["content" => []]]];
+            while($row = $result->fetch_object()){
+                array_push($res_arr['result']['data']['content'], $row);
+            }
+            $res_arr['result']['data']['problemCount'] = count($res_arr['result']['data']['content']);
+            $stmt->close();
+            $mysqli->close();
+            return $res_arr;
+        }else{
+            $stmt->close();
+            $mysqli->close();
+            return get_error_arr("DB query failed");
+        }
+    }catch(mysqli_sql_exception $e){
+        $stmt->close();
+        $mysqli->close();
+        return get_error_arr("DB error while adding tag!");
+    }
+}
 
 function get_problems_by_tags($user_name, $filter, $offset){
     $config = get_config();
     $path = $config['api_endpoint'] . "tags/problems?limit=20&offset=".$offset."&filter=". $filter;
     $response = (array)json_decode(make_api_request($user_name, $path));
     return $response;
+}
+
+function get_private_tags_for_problem($username, $problemCode){
+    include __DIR__."/../util/dbconnect.php";
+    if($mysqli->connect_error){
+        die("Connection failed: " . $mysqli->connect_error);
+        return get_error_arr("DB connection failed");
+    }
+    try{
+        $query = 
+            "SELECT tag 
+                FROM tags 
+                WHERE 
+                    username=? AND
+                    tagId IN 
+                    (SELECT tagId FROM problems WHERE problemCode=?);";
+        $stmt = $mysqli->prepare($query);
+        $stmt->bind_param("ss", $username, $problemCode);
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+            $res_arr = ["status"=>"OK", "result"=>["tags" => []]];
+            while($row = $result->fetch_object()){
+                array_push($res_arr['result']['tags'], $row->tag);
+            }
+            $res_arr['result']['tagCount'] = count($res_arr['result']['tags']);
+            $stmt->close();
+            $mysqli->close();
+            return $res_arr;
+        }else{
+            $stmt->close();
+            $mysqli->close();
+            return get_error_arr("DB query failed");
+        }
+    }catch(mysqli_sql_exception $e){
+        $stmt->close();
+        $mysqli->close();
+        return get_error_arr("DB error while adding tag!");
+    }
 }
--- src/routes/tags.php
+++ src/routes/tags.php
@@ -50,9 +50,21 @@ $app->get('/api/tags/problems/{username}', function ($request, $response, $args)
         ->withHeader('Content-Type', 'application/json');
 });
 
-// Gets problemCode with the private tags
-$app->post('/api/tags/my/problems/{username}', function ($request, $response, $args) {
-    
+// Gets problems with the private tags
+$app->get('/api/tags/problems/my/{username}', function ($request, $response, $args) {
+    $tags = $request->getQueryParams();
+    $payload = get_problems_by_private_tags($args['username'], $tags['filter'], $tags['offset']);
+    if(array_key_exists("status", $payload) && $payload["status"] != "OK"){
+        $response->getBody()->write(json_encode($payload));
+        return $response
+            ->withStatus(500)
+            ->withHeader('Content-Type', 'application/json');
+    }
+    $response->getBody()->write(json_encode($payload));
+    return $response
+        ->withStatus(200)
+        ->withHeader('Content-Type', 'application/json');
+
 });
 
 // Create/Add a private tag to a problem
@@ -61,8 +73,16 @@ $app->post('/api/tags/{username}', function ($request, $response, $args) {
     
     $tag = create_private_tag($args['username'], $body['tag']);
     if(array_key_exists('status', $tag) && $tag['status'] != "OK"){
+        // If tag already exists
         if(array_key_exists('error', $tag) && $tag['error'] == "Tag already exists"){
-            $problem = add_tag_to_problem($args['username'], $body['problemCode'], $body['tag']);
+            $problem = add_tag_to_problem(
+                $args['username'], 
+                $body['problemCode'], 
+                $body['tag'],
+                $body['successfulSubmissions'], 
+                $body['totalSubmissions'], 
+                $body['problemTags']
+            );
             if(array_key_exists("status", $problem) && $problem["status"] != "OK"){
                 $response->getBody()->write(json_encode($problem));
                 return $response
@@ -80,7 +100,15 @@ $app->post('/api/tags/{username}', function ($request, $response, $args) {
             ->withHeader('Content-Type', 'application/json');
     }
     
-    $problem = add_tag_to_problem($args['username'], $body['problemCode'], $body['tag']);
+    // If key doesn't exists
+    $problem = add_tag_to_problem(
+        $args['username'], 
+        $body['problemCode'], 
+        $body['tag'], 
+        $body['successfulSubmissions'], 
+        $body['totalSubmissions'], 
+        $body['problemTags']
+    );
     if(array_key_exists("status", $problem) && $problem["status"] != "OK"){
         $response->getBody()->write(json_encode($problem));
         return $response
@@ -88,6 +116,21 @@ $app->post('/api/tags/{username}', function ($request, $response, $args) {
             ->withHeader('Content-Type', 'application/json');
     }
     $response->getBody()->write(json_encode($tag));
+    return $response
+        ->withStatus(200)
+        ->withHeader('Content-Type', 'application/json');
+});
+
+// Get private tags for a problemCode
+$app->get('/api/tags/{problemCode}/my/{username}', function ($request, $response, $args) {
+    $payload = (get_private_tags_for_problem($args['username'], $args['problemCode']));
+    if(array_key_exists("status", $payload) && $payload["status"] != "OK"){
+        $response->getBody()->write(json_encode($payload));
+        return $response
+            ->withStatus(500)
+            ->withHeader('Content-Type', 'application/json');
+    }
+    $response->getBody()->write(json_encode($payload));
     return $response
         ->withStatus(200)
         ->withHeader('Content-Type', 'application/json');
